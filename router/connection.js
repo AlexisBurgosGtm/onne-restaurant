eaca2483467a3e44bccf5bcc330273a738f939e8
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-01-25 15:07:55 -0600

    primer prueba online, mapa en cliente

--- router/connection.js
+++ router/connection.js
@@ -1,8 +1,8 @@
-﻿//const config = {user: 'DB_A45479_EXPRESS_admin',password: 'razors1805',server: 'sql7002.site4now.net',database: 'DB_A45479_EXPRESS',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
+﻿const config = {user: 'DB_A45479_EXPRESS_admin',password: 'razors1805',server: 'sql7002.site4now.net',database: 'DB_A45479_EXPRESS',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
 //const config = {user: 'iEx', password: 'iEx', server: '192.168.0.200', database: 'VENTAS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 //const config = {user: 'iEx', password: 'iEx', server: 'ONNE\\SQL2014', database: 'VENTAS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 
-const config = {user: 'iEx', password: 'iEx', server: 'ONNE\\SQLDEVELOPMENT', database: 'DB_A45479_EXPRESS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
+//const config = {user: 'iEx', password: 'iEx', server: 'ONNE\\SQLDEVELOPMENT', database: 'DB_A45479_EXPRESS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 //const config = {user: 'iEx', password: 'iEx', server: 'ONNE\\SQLDEVELOPMENT', database: 'VENTAS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 
 const sql = require('mssql');
